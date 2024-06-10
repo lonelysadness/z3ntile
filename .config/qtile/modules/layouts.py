296bb0e80a11229cc76327c956aaddf39b085daa
lonelysadness <lonelysadness@proton.me>
2024-06-10 17:16:51 +0200

    minor changes

--- .config/qtile/modules/layouts.py
+++ .config/qtile/modules/layouts.py
@@ -3,19 +3,18 @@ from libqtile import layout
 # Define common layout properties
 common_layout_properties = {
     "border_focus": "#ebdbb2",
-    "border_normal": "#ebdbb2",
+    "border_normal": "#666666",
     "margin": 4
 }
 
 # Special case for layouts that also need 'border_width'
 border_layout_properties = {**common_layout_properties, "border_width": 2}
 
-# Defining layouts with the common properties
+# Defining layouts
 layouts = [
     layout.Columns(**border_layout_properties),
     layout.Tile(**border_layout_properties),
-    layout.Max(**border_layout_properties),
-    layout.Floating(**border_layout_properties),
+    layout.Max(**common_layout_properties),
+    layout.Floating(**common_layout_properties),
 ]
-
 
